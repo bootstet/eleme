dc43bae859a6b682feadb6b54ccc03b63ce22ca4
bootstet <bootstet@163.com>
2017-06-08 16:57:31 +0800

    vue 饿了么项目

---
+++ src/components/header/header.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="header">
+    我是header
+  </div>
+</template>
+<script type="text/ecamscript-6">
+  export default{};
+</script>
+<!-- <style lang="sass" rel="stylesheet/stylus"> -->
+<style>
+
+</style>
